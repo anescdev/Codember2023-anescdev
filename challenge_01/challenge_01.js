47ca0a2c82f61a0edcd61f418828563e2df9c6b3
anescdev <anescdev@gmail.com>
2023-11-04 22:50:59 +0100

    Readme y cambio al ch_01

--- challenge_01/challenge_01.js
+++ challenge_01/challenge_01.js
@@ -1,21 +1,26 @@
+// Mapa donde iremos guardando las palabras encontradas secuencialmente
+// La clave será la palabra del fichero y el valor el número de veces que ha sido encontrado
 const words = new Map();
+// Obtengo los datos del mensaje y transformo su contenido para que pueda se procesado correctamente
 const secretMessage = await fetch("https://codember.dev/data/message_01.txt")
     .then(val => val.text())
     .then(val => val.substring(0, val.length-1))
     .then(val=> val.toLowerCase())
     .then(val => val.split(" "));
-//const secretMessage = "casas casa casasas".toLowerCase().split(" ");
+// Recorremos las palabras
 secretMessage.forEach(act=>{
+    // Obtenemos el valor del mapa
     const sum = words.get(act);
     if(sum!=undefined){
+        // Si es diferente a undefined quiere decir que se encontró ya un valor en el mapa por lo que se le sumará uno.
         words.set(act, sum+1);
     }  
     else{
+        // En caso de que sea undefined se añadirá la palabra encontrada al mapa con el valor 1.
         words.set(act, 1)
     }
 });
-let res = "";
-words.forEach((val,key)=>{
-    res = res.concat(`${key}${val}`);
-})
-console.log(res);
+// Creamos un array a partir de las entradas del mapa que son devuelta como arrays de dos posiciones, la primera
+// la clave y la segunda el valor. Se mapea a string con la clave(palabra) y el valor(veces repetida) y unimos
+// el array sin separador.
+console.log(new Array(words.entries()).map(([key, val]) => `${key}${val}`).join(""));
